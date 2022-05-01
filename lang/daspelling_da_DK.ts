b11f22cace82df62aa2351b1a388f02023595972
David Lamhauge <davidlamhauge@gmail.com>
2022-05-01 18:26:19 +0200

    Translation works from own dir. Extra commit

---
+++ lang/daspelling_da_DK.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da_DK">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="21"/>
+        <source>The fun starts here...</source>
+        <translation>Det sjove sterter her...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>Danish</source>
+        <translation>Dansk</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <source>English</source>
+        <translation>Engelsk</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <source>active</source>
+        <translation>aktive</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>/</source>
+        <translation>/</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>max</source>
+        <translation>maks</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <source>Play sound</source>
+        <translation>Afspil ord</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>Previous</source>
+        <translation>Forrige</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source>Next </source>
+        <translation>Næste </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="186"/>
+        <source>Load directory</source>
+        <translation>Hent filer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="209"/>
+        <source>Shuffle list</source>
+        <translation>Bland liste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>Reset list</source>
+        <translation>Nulstil liste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="264"/>
+        <source>Start spelling</source>
+        <translation>Start stavning</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="281"/>
+        <source>Finish spelling</source>
+        <translation>Afslut stavning</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <source>Close</source>
+        <translation>Luk</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="329"/>
+        <source>The word...</source>
+        <translation>Ordet...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="347"/>
+        <source>Your spelling...</source>
+        <translation>Din stavning...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>Open sound file</source>
+        <translation>Åben lydfil</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <source>Sound Files (*.wav)</source>
+        <translation>Lydfiler (*.wav)</translation>
+    </message>
+</context>
+</TS>
---
+++ lang/daspelling_en.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="21"/>
+        <source>The fun starts here...</source>
+        <translation>The fun starts here...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>Danish</source>
+        <translation>Danish</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <source>active</source>
+        <translation>active</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>/</source>
+        <translation>/</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>max</source>
+        <translation>max</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <source>Play sound</source>
+        <translation>Play sound</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>Previous</source>
+        <translation>Previous</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source>Next </source>
+        <translation>Next </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="186"/>
+        <source>Load directory</source>
+        <translation>Load directory</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="209"/>
+        <source>Shuffle list</source>
+        <translation>Shuffle list</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>Reset list</source>
+        <translation>Reset list</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="264"/>
+        <source>Start spelling</source>
+        <translation>Start spelling</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="281"/>
+        <source>Finish spelling</source>
+        <translation>Finish spelling</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <source>Close</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="329"/>
+        <source>The word...</source>
+        <translation>The word...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="347"/>
+        <source>Your spelling...</source>
+        <translation>Your spelling...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>Open sound file</source>
+        <translation>Open sound file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <source>Sound Files (*.wav)</source>
+        <translation>Sound Files (*.wav)</translation>
+    </message>
+</context>
+</TS>
