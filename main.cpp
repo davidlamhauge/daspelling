016461f338a5df8cbf0eb779d021b9736a754a4d
David Lamhauge <davidlamhauge@gmail.com>
2022-05-01 18:25:44 +0200

    Translation works from own dir

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QTranslator translator;
-    translator.load(":lang/daspelling_da_DK");
+    translator.load(":lang/lang/daspelling_da_DK");
     a.installTranslator(&translator);
 
     MainWindow w;
