8a2ed47b291473d6cc2cf0054e51f27362543ac7
David Lamhauge <davidlamhauge@gmail.com>
2022-05-08 16:38:35 +0200

    Edited yml file once again

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -121,7 +121,7 @@ void MainWindow::getFileList()
 
 void MainWindow::play()
 {
-    // mediaplayer
+    // mediaplayer lives goes on
     player->setMedia(QUrl::fromLocalFile(mLastDir+ "/" + mFileList.at(mActiveSound)));
     player->play();
 
