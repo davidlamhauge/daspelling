45c9a0adcceaff693f14d67456a55a02bc4c1957
David <davidlamhauge@gmail.com>
2022-05-08 10:08:02 +0200

    Update nightly-build-upload.py

--- .github/actions/upload-package/nightly-build-upload.py
+++ .github/actions/upload-package/nightly-build-upload.py
@@ -1,5 +1,5 @@
 [InternetShortcut]
-URL=https://github.com/davidlamhauge/pencil/blob/master/.github/actions/upload-package/nightly-build-upload.py
+URL=https://github.com/davidlamhauge/daspelling/blob/master/.github/actions/upload-package/nightly-build-upload.py
 IDList=
 HotKey=0
 IconFile=C:\Users\david\AppData\Local\Mozilla\Firefox\Profiles\1qbo90sf.default-release-1617025310240\shortcutCache\rIN0S1ChabCv0ry68XUNjA==.ico
