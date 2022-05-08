db0c9279a587c5fd65f6c7987830a0607974c0f8
David Lamhauge <davidlamhauge@gmail.com>
2022-05-08 16:34:52 +0200

    Edited yaml file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -121,7 +121,7 @@ void MainWindow::getFileList()
 
 void MainWindow::play()
 {
-    // mediaplayer spiller fil
+    // mediaplayer
     player->setMedia(QUrl::fromLocalFile(mLastDir+ "/" + mFileList.at(mActiveSound)));
     player->play();
 
