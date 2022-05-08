362aa595c6511ab385dcf057b4407083e358df29
David <davidlamhauge@gmail.com>
2022-05-08 10:08:25 +0200

    Update pr-build-upload.py

--- .github/actions/upload-package/pr-build-upload.py
+++ .github/actions/upload-package/pr-build-upload.py
@@ -1,5 +1,5 @@
 [InternetShortcut]
-URL=https://github.com/davidlamhauge/pencil/blob/master/.github/actions/upload-package/pr-build-upload.py
+URL=https://github.com/davidlamhauge/daspelling/blob/master/.github/actions/upload-package/pr-build-upload.py
 IDList=
 HotKey=0
 IconFile=C:\Users\david\AppData\Local\Mozilla\Firefox\Profiles\1qbo90sf.default-release-1617025310240\shortcutCache\Fc2zBNOb6OyGh1nWXXFwgA==.ico
