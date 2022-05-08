99185e6184cca414aa948ac73ca5f751deee2527
David <davidlamhauge@gmail.com>
2022-05-08 10:07:20 +0200

    Update setup-environment.sh

--- .github/actions/setup-environment/setup-environment.sh
+++ .github/actions/setup-environment/setup-environment.sh
@@ -1,5 +1,5 @@
 [InternetShortcut]
-URL=https://github.com/davidlamhauge/pencil/blob/master/.github/actions/setup-environment/setup-environment.sh
+URL=https://github.com/davidlamhauge/daspelling/blob/master/.github/actions/setup-environment/setup-environment.sh
 IDList=
 HotKey=0
 IconFile=C:\Users\david\AppData\Local\Mozilla\Firefox\Profiles\1qbo90sf.default-release-1617025310240\shortcutCache\pg+YYnD_qKdM5w6KExeI6w==.ico
